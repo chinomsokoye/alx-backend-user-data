cef69fa15aa0418c23390059bd938091161a3aae
nomso <chinomsokoye@gmail.com>
2023-02-02 13:22:52 +0000

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """ Use of regex in replacing occurrences of certain field values """
-import mysql.connector
-import os
-'''
 import re
 from typing import List
 import logging
+import mysql.connector
+import os
 
 
 class RedactingFormatter(logging.Formatter):
@@ -25,8 +24,8 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
+
 PII_FIELDS = ("name", "email", "password", "ssn", "phone")
-'''
 
 
 def get_db() -> connection.MYSQLConnection:
@@ -40,7 +39,6 @@ def get_db() -> connection.MYSQLConnection:
     return db_connect
 
 
-'''
 def get_logger() -> logging.Logger:
     """ Returns a logging.Logger object """
     logger = logging.getLogger("user_data")
@@ -66,6 +64,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return message
 
 
+'''
 def main() -> None:
     """ Obtain database connection using get_db
     retrieve all role in the users table and display
