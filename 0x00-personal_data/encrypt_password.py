e2018805d4ae066ffc76b0509fbe2cf7cae9597e
nomso <chinomsokoye@gmail.com>
2023-02-02 15:28:26 +0000

    Validate provided password matches hashed_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,10 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ Returns byte string password """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Implement is_valid to validate provided password
+    matched hashed_password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
