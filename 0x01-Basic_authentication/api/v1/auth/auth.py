91ad8039a5ada1ed1f8efb11ce5e55d8b5a299d0
nomso <chinomsokoye@yahoo.com>
2023-02-08 13:19:57 +0000

    Repair auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,27 +14,28 @@ class Auth():
         """ require authentication """
         if path is None or excluded_paths is None or not len(excluded_paths):
             return True
+
         if path[-1] != '/':
             path += '/'
         if excluded_paths[-1] != '/':
             excluded_paths += '/'
-        asters = [stars[:-1]
-                  for stars in excluded_paths if stars[-1] == '*']
 
-        for starts in asters:
+        astericks = [stars[:1]
+                     for stars in excluded_paths if stars[-1] == '*']
+
+        for stars in astericks:
             if path.startswith(stars):
                 return False
+
         if path in excluded_paths:
             return False
         return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header """
-        if request is None:
+        if request is None or 'Authorization' not in request.headers:
             return None
-        if 'Authorization' not in request.headers:
-            return None
-        return request.headers['Authorization']
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current user """
