1b1eeb30ff262cc5f648d5766c11a3dcdc963d04
nomso <chinomsokoye@yahoo.com>
2023-02-09 19:46:49 +0000

    Update appropriate file(s)

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
