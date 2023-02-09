8accc6f367b721b7a4da8e48bd5ce671e2ed80c3
nomso <chinomsokoye@yahoo.com>
2023-02-09 22:30:19 +0000

    Implement session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Manage API authentication system
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth():
@@ -40,3 +41,10 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ Flask request object """
         return None
+
+    def session_cookie(self, request=None):
+        """ Returns request cookie value """
+        if request is None:
+            return None
+        cookie = getenv('SESSION_NAME')
+        return request.cookies.get(cookie)
