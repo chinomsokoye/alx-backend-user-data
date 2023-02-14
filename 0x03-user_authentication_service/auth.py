eed8c4832a18031e64c71deb118a0a2bdbe5df03
nomso <chinomsokoye@yahoo.com>
2023-02-14 15:10:19 +0000

    Implement various methods applicable to authentication (user / credentials)

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,7 +56,7 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            if bcrypt.checkpw(password.encode('utf-5'), user.hashed_password):
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
                 return True
         except NoResultFound:
             pass
