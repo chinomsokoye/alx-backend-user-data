0d29aa0352b2f655c574b83f64d2c0f428489f6a
nomso <chinomsokoye@yahoo.com>
2023-02-14 12:37:38 +0000

    Implement the method add_user in the DB class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchmey.exc import InvalidRequestError
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
